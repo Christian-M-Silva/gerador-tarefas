7f16fc3d16cf6ba15482de3434b2ed5661b196a2
Christian Silva <tininjacms@gmail.com>
2025-03-03 07:22:49 -0300

    feat(addTaks): Add opção para add task se existir arquivo, add a conversão de arquivo

--- tarefas.js
+++ tarefas.js
@@ -1,16 +1,31 @@
-import fs from 'fs';
+import fs from 'fs/promises';
+import { v4 as uuidv4 } from 'uuid';
+const path = 'tarefas.json'
 
-export function addTask(task = "Teste") {
-    const existFile = fs.existsSync('tarefas.json')
-    console.log("🚀 ~ addTask ~ existFile:", existFile)
-    
+
+export async function addTask(task = "Teste") {
+    const existFile = await fs.access(path).then(() => true).catch(() => false);
+
 
     if (existFile) {
+        const jsonObj = await convertFileJsonToJson()
+        jsonObj.push(
+            {
+                status: 'pendente',
+                task,
+                id: uuidv4(),
+            }
+        )
+
+        const jsonString = JSON.stringify(jsonObj)
+
+        fs.writeFile(path, jsonString, 'utf-8').then(() => console.log('Tarefa adicionada com sucesso!!!')).catch((err) => { throw err });
         
     }
+}
 
-    // fs.appendFile('tarefas.json', task , function (err) {
-    //     if (err) throw err;
-    //     console.log('Saved!');
-    //   });
+async function convertFileJsonToJson() {
+    const json = await fs.readFile(path, 'utf8').then((result) => JSON.parse(result)).catch((err) => { throw err });
+
+    return json
 }
